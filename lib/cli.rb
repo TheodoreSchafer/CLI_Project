c818e9bb11f9c50790a115aff5b75bafe9c0a5b0
TheodoreSchafer <theodore.schafer@gmail.com>
2020-10-02 11:50:33 -0400

    third save

---
+++ lib/cli.rb
@@ -0,0 +1,13 @@
+class CLI
+
+    def start 
+        puts ""
+        puts "Welcome to Quote!"
+        puts ""
+        puts "Would you like an amazing quote?"
+        puts ""
+        quote = gets.chomp.downcase
+        binding.pry
+
+    end 
+end 
