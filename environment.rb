ec01979d82d3524a4c81cd979db3921b4054a6ef
TheodoreSchafer <theodore.schafer@gmail.com>
2020-10-02 11:17:45 -0400

    second save

---
+++ environment.rb
@@ -0,0 +1,10 @@
+#connect program so our files can talk tot one another 
+#require all gems we need  "first save"
+
+require 'pry'
+require 'net/http'
+require 'json'
+
+require_relative './lib/api'
+require_relative './lib/cli'
+require_relative './lib/quote'
